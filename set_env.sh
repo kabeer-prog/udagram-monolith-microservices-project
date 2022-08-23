72249ebb76bfaab40feffa020d8d0d93833d87c4
kabeer-prog <codekabeer@DESKTOP-GO7PRNU.localdomain>
2022-08-23 21:21:45 +0100

    added starter files and node modules

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramdata
+export POSTGRES_PASSWORD=udagramdata
+export POSTGRES_HOST=udagramdata.ckhnestvbko6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdata
+export AWS_BUCKET=udakabeers3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
